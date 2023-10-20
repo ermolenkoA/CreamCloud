970b5f7f5fd7e4b430f182879906c437bf87a3b2
ermolenkoA <kypalo2012@gmail.com>
2023-10-20 06:52:45 +0300

    Designed enter number screen and added app themes

--- app/src/main/java/com/example/creamcloud/ui/EnterNumberFragment.kt
+++ app/src/main/java/com/example/creamcloud/ui/EnterNumberFragment.kt
@@ -1,7 +1,18 @@
 package com.example.creamcloud.ui
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.creamcloud.R
 
 class EnterNumberFragment : Fragment() {
 
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_enter_number, container, false)
+    }
 }
---
+++ app/src/main/java/com/example/creamcloud/views/PhoneEditText.kt
@@ -0,0 +1,15 @@
+package com.example.creamcloud.views
+
+import android.content.Context
+import android.telephony.PhoneNumberFormattingTextWatcher
+import android.util.AttributeSet
+import android.widget.EditText
+import androidx.appcompat.widget.AppCompatEditText
+
+class PhoneEditText @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+): AppCompatEditText(context, attrs, defStyleAttr) {
+
+}
